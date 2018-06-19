00063525617a4a791dfaced56d9be9cfbe3f16ee
Rachele Morino <rachelekm@gmail.com>
2018-06-18 21:42:02 -0400

    creating test files, all smoke tests

---
+++ src/components/feedback.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Feedback from './feedback';
+
+describe('<Feedback/>', () => {
+  it('Renders without crashing', () => {
+    shallow(<Feedback />);
+  });
+
+  it('Renders appropriate feedback', () => {
+
+  });
+
+});
